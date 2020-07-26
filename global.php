906fe08c1cdb48e6d9345cda79aff443aafecaaf
surveyor <matingonk@gmail.com>
2020-07-26 19:27:13 +0200

    global covid stats coming soon

--- global.php
+++ global.php
@@ -4,5 +4,9 @@
 	<h4 class="alert-heading">UNDER CONSTRUCTION</h4>
 	We are currently working on providing Statistical Data at a Global Level. Please come back soon
 </div>
+<div class="alert alert-info" role="alert" style="text-align:center;">
+	<h4 class="alert-heading"> <i class="fa fa-github" aria-hidden="true"></i> </h4>
+	You are also free to come and contribute on <a href="https://github.com/Sureyor-Jr/COVID-19-DataCenter/">GitHub Repository</a>
+</div>
 
 <?php require_once('footer.php');?>
