bcd34a489a17caea41540a2ac1191d8c2e289187
surveyor <matingonk@gmail.com>
2020-08-18 19:18:13 +0200

    added isolation center finder app

---
+++ isolation.php
@@ -0,0 +1,23 @@
+<?php require_once('header.php');?>
+
+						<div class="col-12">
+                            <div class="card card-bordered">
+                               <p>This finder tool is still being developed therefore you might not find any Isolation Centers in the province that you are currently in. Please bare with us as we are still waiting for more updates on the confirmed locations <i class="fa fa-location-arrow" aria-hidden="true"></i></p>
+                                <div class="card-body">
+								<iframe width="100%" height="500" frameborder="0" scrolling="no" allowfullscreen src="https://arcg.is/0DazzP"></iframe>
+                                    
+                                </div>
+                            </div>
+                        </div>
+						<br>
+						<div class="col-12">
+                            <div class="card card-bordered">
+                               <div class="card-body">
+								Our developers would also like to hear about your experiences & suggestions on this tool. Kindly send them through via <a href="https://wa.me/263718870/">WhatsApp <i class="fa fa-whatsapp" aria-hidden="true"></i></a>, <a href="mailto:admin@africansurveyors.net">Email</a> or you can fill in the <a href="#">feedback form</a>.
+                                    
+                                </div>
+                            </div>
+                        </div>
+						
+
+<?php require_once('footer.php');
