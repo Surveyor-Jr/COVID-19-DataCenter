7cd4b7a9407bd058f91cb2db51d55c4db1d67d15
surveyor <matingonk@gmail.com>
2020-08-19 11:54:37 +0200

    testing sites application coming soon

---
+++ testing.php
@@ -0,0 +1,10 @@
+<?php require_once('header.php');?>
+
+<div class="alert alert-info" role="alert" style="text-align:center">
+	<h4 class="alert-heading">COMING SOON</h4>
+	<p>We are still gathering the locations of all testing sites in Zimbabwe. We will update you as soon as they are available. </p>
+	<hr>
+	
+</div>
+
+<?php require_once('footer.php');?>
