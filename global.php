3d4096ff999a4f61c59359b61553a2f290bd1f70
surveyor <matingonk@gmail.com>
2020-08-19 09:57:09 +0200

    added Johns Hopkins University Dashboard

--- global.php
+++ global.php
@@ -1,12 +1,9 @@
 <?php require_once('header.php');?>
 
-<div class="alert alert-info" role="alert" style="text-align:center;">
-	<h4 class="alert-heading">UNDER CONSTRUCTION</h4>
-	We are currently working on providing Statistical Data at a Global Level. Please come back soon
+<div class="alert alert-primary" role="alert">
+	This is the official COVID-19 Dashboard developed by <a href="https://systems.jhu.edu/" class="alert-link">Johns Hopkins University Center for Systems Science and Engineering (JHU CSSE)</a>.
 </div>
-<div class="alert alert-info" role="alert" style="text-align:center;">
-	<h4 class="alert-heading"> <i class="fa fa-github" aria-hidden="true"></i> </h4>
-	You are also free to come and contribute on <a href="https://github.com/Sureyor-Jr/COVID-19-DataCenter/">GitHub Repository</a>
-</div>
+
+<style>.embed-container {position: relative; padding-bottom: 80%; height: 0; max-width: 100%;} .embed-container iframe, .embed-container object, .embed-container iframe{position: absolute; top: 0; left: 0; width: 100%; height: 100%;} small{position: absolute; z-index: 40; bottom: 0; margin-bottom: -15px;}</style><div class="embed-container"><iframe width="500" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" title="COVID-19" src="https://www.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6"></iframe></div>
 
 <?php require_once('footer.php');?>
